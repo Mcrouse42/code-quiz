78065c070510cb7c60d935957139402b9c830b5c
Mcrouse <matthew.crouse@temple.edu>
2021-03-23 15:07:59 -0400

    added question array to script

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+// Array for questions
+var questions = [
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["1. <script>", "2. <js>", "3. <javascript>", "4. <scripting>"],
+    answer: "1. <script>",
+  },
+  {
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: [
+      "1. The <head> section",
+      "2. Both the <head> and <body> section",
+      "3. The <body> section",
+    ],
+    answer: "3. The <body> section",
+  },
+  {
+    title: "How do you create a function in JavaScript?",
+    choices: [
+      "1. function: myFunction()",
+      "2. function myFunction()",
+      "3. function = myFunction()",
+    ],
+    answer: "3. function = myFunction()",
+  },
+  {
+    title: "How do you call a function named 'myFunction'? ",
+    choices: [
+      "1. call myFunction()",
+      "2. myFunction()",
+      "3. Call function myFunction()",
+    ],
+    answer: "2. myFunction()",
+  },
+  {
+    title: "How does a for loop start?",
+    choices: [
+      '1. for (i<=5; i++)',
+      '2. for i =1 to 5',
+      '3. for (i = 0; i<= 5)',
+      '4. for (i=0; i<=5; i++)',
+    ],
+    answer: '4. for (i=0; i<=5; i++)',
+  },
+];
